d19f06d4ee48b1f388b44ccb743bd6535b5c3365
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-03-11 19:17:46 -0700

    first commit

---
+++ test/firstTest.js
@@ -0,0 +1,19 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const safari = require('selenium-webdriver/safari');
+const assert = require('assert');
+
+let driver = new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).setChromeOptions().setSafariOptions().build();
+
+(async function navigateToNbaLogin() {
+  //let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.navigate().to('https://www.nba.com/');
+    await driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
+    await driver.findElement(By.xpath("//a[contains(text(),'Sign in with NBA ID')]")).click();
+    await driver.wait(until.titleIs('NBA Login | NBA.com'), 5000);
+  } finally {
+    await driver.quit();
+  }
+})();
