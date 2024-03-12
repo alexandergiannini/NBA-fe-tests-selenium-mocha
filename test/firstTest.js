bb061fd9cdfff3a53359658b271edd919b774319
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-03-11 20:05:27 -0700

    added assertion

--- test/firstTest.js
+++ test/firstTest.js
@@ -6,13 +6,20 @@ const assert = require('assert');
 
 let driver = new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).setChromeOptions().setSafariOptions().build();
 
-(async function navigateToNbaLogin() {
-  try {
-    await driver.navigate().to('https://www.nba.com/');
-    await driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
-    await driver.findElement(By.xpath("//a[contains(text(),'Sign in with NBA ID')]")).click();
-    await driver.wait(until.titleIs('NBA Login | NBA.com'), 5000);
-  } finally {
-    await driver.quit();
-  }
-})();
+
+describe("Navigate to NBA Login test", () => {
+  it("should take me to the login page", () => {
+    (async function navigateToNbaLogin() {
+      try {
+      await driver.navigate().to('https://www.nba.com/');
+      await driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
+      await driver.findElement(By.xpath("//a[contains(text(),'Sign in with NBA ID')]")).click();
+      await driver.wait(until.titleIs('NBA Login | NBA.com'), 5000);
+      let signInText = driver.findElement(By.className(".SignIn_title__l6maW")).getText();
+      assert.strictEqual(signInText, 'Sign in with your NBA IDd');
+    } finally {
+      await driver.quit();
+    }
+  })();
+})
+})
